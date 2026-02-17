c812b9205a840841c900eca9ed51738e40fbf354
micaellaa <99943347+micaellaa@users.noreply.github.com>
2026-02-17 14:51:12 +0800

    Add basic router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
-import MainLayout from './layouts/MainLayout/MainLayout'
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
+import { theme } from "./theme";
+import LoginForm from './pages/Authentication/LoginPage/LoginPage';
+import RegisterForm from './pages/Authentication/RegisterPage/RegisterPage';
 
 function App() {
 
   return (
-    <>
-      <MainLayout>
-        lon
-      </MainLayout>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route index element={<Navigate to="login" replace />} />
+
+        <Route path="login" element={<LoginForm />} />
+        <Route path="register" element={<RegisterForm />} />
+      </Routes>
+    </ThemeProvider>
   )
 }
 
