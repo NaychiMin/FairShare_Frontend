f53e2aab740a8197aca41654b5c1087359534fe3
micaellaa <99943347+micaellaa@users.noreply.github.com>
2026-02-17 17:55:07 +0800

    Add tailwind css config
    specific to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
 
-import { theme } from "./theme";
 import LoginForm from './pages/Authentication/LoginPage/LoginPage';
 import RegisterForm from './pages/Authentication/RegisterPage/RegisterPage';
+import MainLayout from './layouts/MainLayout';
+import DashboardPage from './pages/Dashboard/DashboardPage';
+import ProtectedRoute from './context/Authentication/ProtectedRoute';
 
 function App() {
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Routes>
-        <Route index element={<Navigate to="login" replace />} />
+    <Routes>
+      <Route index element={<Navigate to="login" replace />} />
 
-        <Route path="login" element={<LoginForm />} />
-        <Route path="register" element={<RegisterForm />} />
-      </Routes>
-    </ThemeProvider>
+      <Route path="login" element={<LoginForm />} />
+      <Route path="register" element={<RegisterForm />} />
+
+      
+      <Route element={<ProtectedRoute />}> {/* routes that are login-protected */}
+        <Route element={<MainLayout />}> {/* routes WITH MainLayout */}
+          <Route path="dashboard" element={<DashboardPage />} />
+        </Route>
+      </Route>
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 import App from './App.tsx'
+import './index.css'
+
+import { theme } from "./theme";
 import { BrowserRouter } from 'react-router-dom'
 
 import { AuthProvider } from './context/Authentication/AuthProvider.tsx'
@@ -10,7 +14,10 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
