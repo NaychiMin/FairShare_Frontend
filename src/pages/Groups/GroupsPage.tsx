f8908dc18d4c38318031004fe90a563d6b112299
shareefa14 <ksitishareefa14@gmail.com>
2026-02-20 17:46:27 +0800

    show all groups this user have

--- src/pages/Groups/GroupsPage.tsx
+++ src/pages/Groups/GroupsPage.tsx
@@ -1,10 +1,42 @@
-import { Button, Grid } from "@mui/material";
+import { Button, Divider, Grid, ListItemText, MenuItem, MenuList } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import groupService from "../../services/groupService";
+import { useAuth } from "../../context/Authentication/useAuth";
 
 const GroupsPage = () => {
   const navigate = useNavigate();
+  const { user, jwtToken } = useAuth();
+  const [groups, setGroups] = useState<any[]>([]);
+  const fetchGroups = async () => {
+    // Only fetch if we have the required data
+    if (!user?.email || !jwtToken) return;
+
+    try {
+      const response = await groupService.getAll(user.email, jwtToken);
+      if (response) {
+        setGroups(response);
+      }
+    } catch (err) {
+      toast.error((err as any).response?.data?.message || "Failed to fetch groups");
+    }
+  };
+  useEffect(() => {
+  fetchGroups(); // Execute it
+}, []);
   return (
-    <Grid ml={2} mt={2}>
+    <Grid size={12} width={'95%'} ml={2} mt={2}>
+      <MenuList>
+        {groups.map((group) => (
+          <>
+          <MenuItem key={group.id} onClick={() => navigate(`/groups/${group.id}`)}>
+            <ListItemText>{group.groupName}</ListItemText>
+          </MenuItem>
+          <Divider />
+          </>
+        ))}
+      </MenuList>
       <Button variant="outlined" onClick={()=>navigate('/newGroup')}>Add New</Button>
     </Grid>
   );
--- src/services/groupService.ts
+++ src/services/groupService.ts
@@ -13,6 +13,15 @@ class GroupService {
     return response.data;
   }
 
+  async getAll(email: any, jwtToken: string) {
+    const response = await axios.get(`/group/all/${email}`, {
+        headers: {
+          'Authorization': `Bearer ${jwtToken}`,
+        },
+      });
+    return response.data;
+  }
+
   getToken() {
     return localStorage.getItem(TOKEN_KEY);
   }
